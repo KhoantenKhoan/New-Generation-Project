370f758cb7f4ed9a4375621191821d7181f69429
sutoan <93644983+sutoan@users.noreply.github.com>
2022-11-14 15:41:09 +0700

    update

--- js/footAndHead.js
+++ js/footAndHead.js
@@ -81,14 +81,6 @@ let humberger = `
             <ul>
                 <li class="active"><a href="./index.html">Trang chủ</a></li>
                 <li><a href="./shop-grid.html">Sản phẩm</a></li>
-                <li><a href="#">Pages</a>
-                    <ul class="header__menu__dropdown">
-                        <li><a href="./shop-details.html">Shop Details</a></li>
-                        <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                        <li><a href="./checkout.html">Check Out</a></li>
-                        <li><a href="./blog-details.html">Blog Details</a></li>
-                    </ul>
-                </li>
                 <li><a href="./blog.html">Blog</a></li>
                 <li><a href="./contact.html">Contact</a></li>
             </ul>
@@ -120,23 +112,8 @@ let header = `
                     </div>
                     <div class="col-lg-6 col-md-6">
                         <div class="header__top__right">
-                            <div class="header__top__right__social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-linkedin"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                            </div>
-                            <div class="header__top__right__language">
-                                <img src="img/language.png" alt="">
-                                <div>English</div>
-                                <span class="arrow_carrot-down"></span>
-                                <ul>
-                                    <li><a href="#">Vietnames</a></li>
-                                    <li><a href="#">English</a></li>
-                                </ul>
-                            </div>
                             <div class="header__top__right__auth">
-                                <a href="#"><i class="fa fa-user"></i> Đăng nhập</a>
+                                <a href="./login.html"><i class="fa fa-user"></i> Đăng nhập</a>
                             </div>
                         </div>
                     </div>
@@ -145,7 +122,7 @@ let header = `
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-lg-3">
+                <div class="col-lg-3 box_logo">
                     <div class="header__logo">
                         <a href="./index.html"><img src="img/logo.png" alt=""></a>
                     </div>
@@ -154,27 +131,11 @@ let header = `
                     <nav class="header__menu">
                         <ul>
                             <li class="active"><a href="./index.html">Trang chủ</a></li>
-                            <li><a href="./shop-grid.html">Sản phẩm</a>
-                                <ul class="header__menu__dropdown">
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="./shop-grid.html">Sản phẩm</a></li>
                             <li><a href="./blog.html">Tin tức</a></li>
                             <li><a href="./contact.html">Liên hệ</a></li>
                         </ul>
                     </nav>
-                </div>
-                <div class="col-lg-3">
-                    <div class="header__cart">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                            <li><a href="./shoping-cart.html"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
-                        </ul>
-                        <div class="header__cart__price">item: <span>$150.00</span></div>
-                    </div>
                 </div>
             </div>
             <div class="humberger__open">
@@ -183,118 +144,59 @@ let header = `
         </div>
 `;
 
-let hero = `
-<div class="container">
-<div class="row">
-    <div class="col-lg-3">
-        <div class="hero__categories">
-            <div class="hero__categories__all">
-                <i class="fa fa-bars"></i>
-                <span>Danh mục</span>
-            </div>
-            <ul>
-                <li><a href="#">Fresh Meat</a></li>
-                <li><a href="#">Vegetables</a></li>
-                <li><a href="#">Fruit & Nut Gifts</a></li>
-                <li><a href="#">Fresh Berries</a></li>
-                <li><a href="#">Ocean Foods</a></li>
-                <li><a href="#">Butter & Eggs</a></li>
-                <li><a href="#">Fastfood</a></li>
-                <li><a href="#">Fresh Onion</a></li>
-                <li><a href="#">Papayaya & Crisps</a></li>
-                <li><a href="#">Oatmeal</a></li>
-                <li><a href="#">Fresh Bananas</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="col-lg-9">
-        <div class="hero__search">
-            <div class="hero__search__form">
-                <form action="#">
-                    <div class="hero__search__categories">
-                        Tất cả danh mục
-                        <span class="arrow_carrot-down"></span>
-                    </div>
-                    <input type="text" placeholder="Bạn muốn tìm gì ?">
-                    <button type="submit" class="site-btn">SEARCH</button>
-                </form>
-            </div>
-            <div class="hero__search__phone">
-                <div class="hero__search__phone__icon">
-                    <i class="fa fa-phone"></i>
-                </div>
-                <div class="hero__search__phone__text">
-                    <h5>+65 11.188.888</h5>
-                    <span>support 24/7 time</span>
-                </div>
-            </div>
-        </div>
-        <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
-            <div class="hero__text">
-                <span>FRUIT FRESH</span>
-                <h2>Vegetable <br />100% Organic</h2>
-                <p>Free Pickup and Delivery Available</p>
-                <a href="#" class="primary-btn">Mua ngay</a>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-`
 
-let heroPage = `
-<div class="container">
-<div class="row">
-    <div class="col-lg-3">
-        <div class="hero__categories">
-            <div class="hero__categories__all">
-                <i class="fa fa-bars"></i>
-                <span>All departments</span>
-            </div>
-            <ul>
-                <li><a href="#">Fresh Meat</a></li>
-                <li><a href="#">Vegetables</a></li>
-                <li><a href="#">Fruit & Nut Gifts</a></li>
-                <li><a href="#">Fresh Berries</a></li>
-                <li><a href="#">Ocean Foods</a></li>
-                <li><a href="#">Butter & Eggs</a></li>
-                <li><a href="#">Fastfood</a></li>
-                <li><a href="#">Fresh Onion</a></li>
-                <li><a href="#">Papayaya & Crisps</a></li>
-                <li><a href="#">Oatmeal</a></li>
-                <li><a href="#">Fresh Bananas</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="col-lg-9">
-        <div class="hero__search">
-            <div class="hero__search__form">
-                <form action="#">
-                    <div class="hero__search__categories">
-                        Tất cả danh mục
-                        <span class="arrow_carrot-down"></span>
-                    </div>
-                    <input type="text" placeholder="What do yo u need?">
-                    <button type="submit" class="site-btn">Tìm kiếm</button>
-                </form>
-            </div>
-            <div class="hero__search__phone">
-                <div class="hero__search__phone__icon">
-                    <i class="fa fa-phone"></i>
-                </div>
-                <div class="hero__search__phone__text">
-                    <h5>+65 11.188.888</h5>
-                    <span>Hỗ trợ 24/7</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-`
+// let heroPage = `
+// <div class="container">
+// <div class="row">
+//     <div class="col-lg-3">
+//         <div class="hero__categories">
+//             <div class="hero__categories__all">
+//                 <i class="fa fa-bars"></i>
+//                 <span>All departments</span>
+//             </div>
+//             <ul>
+//                 <li><a href="#">Fresh Meat</a></li>
+//                 <li><a href="#">Vegetables</a></li>
+//                 <li><a href="#">Fruit & Nut Gifts</a></li>
+//                 <li><a href="#">Fresh Berries</a></li>
+//                 <li><a href="#">Ocean Foods</a></li>
+//                 <li><a href="#">Butter & Eggs</a></li>
+//                 <li><a href="#">Fastfood</a></li>
+//                 <li><a href="#">Fresh Onion</a></li>
+//                 <li><a href="#">Papayaya & Crisps</a></li>
+//                 <li><a href="#">Oatmeal</a></li>
+//                 <li><a href="#">Fresh Bananas</a></li>
+//             </ul>
+//         </div>
+//     </div>
+//     <div class="col-lg-9">
+//         <div class="hero__search">
+//             <div class="hero__search__form">
+//                 <form action="#">
+//                     <div class="hero__search__categories">
+//                         Tất cả danh mục
+//                         <span class="arrow_carrot-down"></span>
+//                     </div>
+//                     <input type="text" placeholder="What do yo u need?">
+//                     <button type="submit" class="site-btn">Tìm kiếm</button>
+//                 </form>
+//             </div>
+//             <div class="hero__search__phone">
+//                 <div class="hero__search__phone__icon">
+//                     <i class="fa fa-phone"></i>
+//                 </div>
+//                 <div class="hero__search__phone__text">
+//                     <h5>+65 11.188.888</h5>
+//                     <span>Hỗ trợ 24/7</span>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+// </div>
+// `
 
 document.querySelector("#footer").innerHTML += footer;
 document.querySelector("#humberger").innerHTML += humberger;
 document.querySelector("#header").innerHTML += header;
-document.querySelector("#hero").innerHTML += hero;
-document.querySelector("#heroPage").innerHTML += heroPage;
+// document.querySelector("#heroPage").innerHTML += heroPage;
