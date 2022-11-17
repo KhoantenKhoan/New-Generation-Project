f8fcc24036adfe59f3e364ffab01e98c58e6cac8
Khoan <phungdang.khoan@gmail.com>
2022-11-17 22:06:26 +0700

    locsp

--- js/danhmuc/hiendanhmuc.js
+++ js/danhmuc/hiendanhmuc.js
@@ -2,7 +2,8 @@ import { FireBaseService } from "../API/firebaseService.js";
 const brand = new FireBaseService();
 
 async function showBrand() {
-  
+    let params = new URLSearchParams(location.search);
+    let id_item = params.get("idDMT");
     let response=await brand.getAll('danhMuc');
     
     let data = await response.json();
@@ -11,31 +12,14 @@ async function showBrand() {
     if (data) {
       for (const [key, value] of Object.entries(data)) {
         if (value) {
-          content += `
-            <li><a href="shop-grid.html?id=${value.id}">${value.tenDM}</a></li>
-          `;
+          if(id_item == value.idDMT){
+            content += `
+              <li><a href="shop-grid-2.html?idDMT=${value.idDMT}&?idDM=${value.id}">${value.tenDM}</a></li>
+            `;
+          }
         }
       }
     }
     document.getElementById('navmenu').innerHTML = content;
   }
   showBrand();
-  import { FireBaseService } from "./firebaseService.js";
-const product = new FireBaseService();
-(async () => {
-let params = new URLSearchParams(location.search);
-let id_item = params.get("id");
-
-var table = document.querySelector("#sach");
-          
-const response = await fetch(
-    `https://silkroad-project-28d19-default-rtdb.asia-southeast1.firebasedatabase.app/danhMuc/${id_item}.json`
-  );
-  const data = await response.json();
-    console.log(data);
-    if(data) {
-      table.innerHTML += `
-      <li><a href="shop-grid.html?id=${value.id}">${value.tenDM}</a></li>
-        `;
-    }
-})();
---
+++ js/sanpham/locsanpham.js
@@ -0,0 +1,58 @@
+import { FireBaseService } from "../API/firebaseService.js";
+const product = new FireBaseService();
+
+async function showProduct() {
+    let params = new URLSearchParams(location.search);
+    let id_item = params.get("idDMT");
+    let id_item1 = params.get("idDM");
+
+    let response=await product.getAll('sanPham');
+    let response1=await product.getAll('danhMuc');
+    
+    let data = await response.json();
+    let data1 = await response1.json();
+
+    let maindata = [''];
+    for(const [key1, value1] of Object.entries(data1)){
+        
+        console.log(id_item);
+        console.log(value1.idDMT);
+        console.log(value1.id);
+
+        if(id_item == value1.idDMT){
+            maindata += value1;
+        }
+    }
+    
+    console.log(maindata);
+
+    let content = ``;
+
+    if (maindata) {
+      for (const [key, value] of Object.entries(maindata)) {
+        if (value) {
+            content += `
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="${value.hinhAnh}">
+                        <a  href="shop-details.html?id=${key}">
+                        <img src="${value.hinhAnh}" alt="">
+                        </a>
+                            <ul class="product__item__pic__hover">
+                            <li><a  onclick="giohang(${key},'${value.tenSP}',${value.gia - (value.gia / value.giamGia)},'${value.hinhAnh}')" href=""><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6><a ref="shop-details.html?id=${key}">${(value.tenSP).toUpperCase()}</a></h6>
+                            <p style="text-decoration: line-through;">${value.gia.replace(/\B(?=(\d{3})+(?!\d))/g, '.')} ₫</p>
+                            <h5 style="color:red;">${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value.gia - (value.gia / value.giamGia))} </h5>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+      }
+    }
+    document.getElementById('locsanpham').innerHTML = content;
+  }
+  showProduct();
