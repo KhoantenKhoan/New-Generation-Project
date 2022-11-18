034f1d9ec5a2161d25f9b60a5ca6f6053b510ba1
Khoan <phungdang.khoan@gmail.com>
2022-11-18 15:20:08 +0700

    a

--- js/sanpham/locsanpham.js
+++ js/sanpham/locsanpham.js
@@ -12,39 +12,28 @@ async function showProduct() {
     let data = await response.json();
     let data1 = await response1.json();
 
-    let maindata = [''];
-    for(const [key1, value1] of Object.entries(data1)){
-        
-        console.log(id_item);
-        console.log(value1.idDMT);
-        console.log(value1.id);
-
-        if(id_item == value1.idDMT){
-            maindata += value1;
-        }
-    }
-    
-    console.log(maindata);
-
     let content = ``;
 
-    if (maindata) {
-      for (const [key, value] of Object.entries(maindata)) {
-        if (value) {
+if (data , data1) {
+    for (const [key, value] of Object.entries(data)) {
+        for(const [key1, value1] of Object.entries(data1)){
+            console.log(value);
+            if(id_item == value1.idDMT){
+        if (value1) {
             content += `
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="product__item">
-                    <div class="product__item__pic set-bg" data-setbg="${value.hinhAnh}">
-                        <a  href="shop-details.html?id=${key}">
-                        <img src="${value.hinhAnh}" alt="">
+                    <div class="product__item__pic set-bg" data-setbg="${value1.hinhAnh}">
+                        <a  href="shop-details.html?id=${key1}">
+                        <img src="${value1.hinhAnh}" alt="">
                         </a>
                             <ul class="product__item__pic__hover">
-                            <li><a  onclick="giohang(${key},'${value.tenSP}',${value.gia - (value.gia / value.giamGia)},'${value.hinhAnh}')" href=""><i class="fa fa-shopping-cart"></i></a></li>
+                            <li><a  onclick="giohang(${key1},'${value1.tenSP}',${value1.gia - (value1.gia / value1.giamGia)},'${value1.hinhAnh}')" href=""><i class="fa fa-shopping-cart"></i></a></li>
                             </ul>
                         </div>
                         <div class="product__item__text">
-                            <h6><a ref="shop-details.html?id=${key}">${(value.tenSP).toUpperCase()}</a></h6>
-                            <p style="text-decoration: line-through;">${value.gia.replace(/\B(?=(\d{3})+(?!\d))/g, '.')} ₫</p>
+                        <h6><a ref="shop-details.html?id=${key1}">${(value1.tenSP).toUpperCase()}</a></h6>
+                            <p style="text-decoration: line-through;">${value1.gia.replace(/\B(?=(\d{3})+(?!\d))/g, '.')} ₫</p>
                             <h5 style="color:red;">${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value.gia - (value.gia / value.giamGia))} </h5>
                     </div>
                 </div>
@@ -52,6 +41,8 @@ async function showProduct() {
             `;
         }
       }
+    }
+    }
     }
     document.getElementById('locsanpham').innerHTML = content;
   }
