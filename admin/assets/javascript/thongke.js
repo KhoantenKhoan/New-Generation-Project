f4bd7168fd8c23ec63f664947a558ee29f55ecee
Khoan <phungdang.khoan@gmail.com>
2022-12-06 09:10:36 +0700

    thongke

--- admin/assets/javascript/thongke.js
+++ admin/assets/javascript/thongke.js
@@ -1,6 +1,26 @@
-const labels = ['January','February','March','April','May','June','July','August','September','October'];
+const labels = [0+" Sản phẩm",10+" Sản phẩm",20+" Sản phẩm",30+" Sản phẩm",40+" Sản phẩm",50+" Sản phẩm",60+" Sản phẩm",70+" Sản phẩm",80+" Sản phẩm",90+" Sản phẩm",100+" Sản phẩm"];
+    let w10 = [];
+    let w20 = [];
+    let w30 = [];
+    let w40 = [];
+    let w50 = [];
+    let w60 = [];
+    let w70 = [];
+    let w80 = [];
+    let w90 = [];
+    let w100 = [];
+    let s10 = [];
+    let s20 = [];
+    let s30 = [];
+    let s40 = [];
+    let s50 = [];
+    let s60 = [];
+    let s70 = [];
+    let s80 = [];
+    let s90 = [];
+    let s100 = [];
 (async () => {
-    let w20 = [{}];
+    
     const response = await fetch(
       "https://silkroad-project-28d19-default-rtdb.asia-southeast1.firebasedatabase.app/sanPham.json"
     );
@@ -9,43 +29,76 @@ const labels = ['January','February','March','April','May','June','July','August
         const row = data1[key];
         for (let i = 1; i < data1.length; i++) {
             if (row) {
-                // console.log(row.luotXem = 99);
-                // console.log(row.luotXem = 99);
-                  if(row.luotXem == 20){
-                          w20 += row; 
-                          console.log(w20);
-                        }
+                if(row.luotXem == 10)w10.push(key) 
+                    
+                if(row.luotXem == 20)w20.push(key) 
+                    
+                if(row.luotXem == 30)w30.push(key) 
+                    
+                if(row.luotXem == 40)w40.push(key) 
+                    
+                if(row.luotXem == 50) w50.push(key) 
+                   
+                if(row.luotXem == 60)w60.push(key) 
+                    
+                if(row.luotXem == 70)w70.push(key) 
+                    
+                if(row.luotXem == 80)w80.push(key) 
+                    
+                if(row.luotXem == 90)w90.push(key) 
+                    
+                if(row.luotXem >= 99)w100.push(key) 
+                
+                if(row.soLuong == 10)s10.push(key) 
+                    
+                if(row.soLuong == 20)s20.push(key) 
+                    
+                if(row.soLuong == 30)s30.push(key) 
+                    
+                if(row.soLuong == 40)s40.push(key) 
+                    
+                if(row.soLuong == 50)s50.push(key) 
+                   
+                if(row.soLuong == 60)s60.push(key) 
+                    
+                if(row.soLuong == 70)s70.push(key) 
+                    
+                if(row.soLuong == 80)s80.push(key) 
+                    
+                if(row.soLuong == 90)s90.push(key) 
+                    
+                if(row.soLuong >= 99)s100.push(key) 
             }
-             return i++;
-        }
-const data = {
+            return i++;
+        }});
+    const data = {
     labels: labels,
     datasets: [
         {
-            label:'Lượt xem',
+            label:'Lượt truy cập',
             backgroundColor: 'blue',
             borderColor: 'blue',
-            data: [0,27,56,45,36,76,56,23,56,25],
+            data: [0,w10.length,w20.length,w30.length,w40.length,w50.length,w60.length,w70.length,w80.length,w90.length,w100.length],
             // tension: 0.4,
         },
    
   
         {
-            label:'Số sản phẩm bán được',
-            backgroundColor: 'yellow',
-            borderColor: 'yellow',
-            data: [0,67,34,65,67,45,46,23,67,98],
-            // tension: 0.4,
-        },
-   
-  
-        {
-            label:'Lượt truy cập',
+            label:'Số lượng sản phẩm',
             backgroundColor: 'red',
             borderColor: 'red',
-            data: [0,68,67,67,78,45,87,56,67,24],
+            data: [0,s10.length,s20.length,s30.length,s40.length,s50.length,s60.length,s70.length,s80.length,s90.length,s100.length],
             // tension: 0.4,
         },
+   
+  
+        // {
+        //     label:'Lượt truy cập',
+        //     backgroundColor: 'red',
+        //     borderColor: 'red',
+        //     data: [0,68,67,67,78,45,87,56,67,24],
+        //     // tension: 0.4,
+        // },
     ],
 }
 const config = {
@@ -55,13 +108,4 @@ const config = {
 const canvas = document.getElementById('canvas');
 const chart = new Chart(canvas, config);
 
-var obj = {"1":5,"2":7,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0}
-var result = Object.keys(obj).map((key) => {
-    let number = Number(key);
-    if(number>5){
-    return number
-    }
-});
-// console.log(result);
-});
 })();
