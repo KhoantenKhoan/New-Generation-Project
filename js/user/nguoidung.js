b65c14c8437522cf8c50120ae235796a6d30f97a
Khoan <phungdang.khoan@gmail.com>
2022-12-08 09:21:24 +0700

    usser

--- js/user/nguoidung.js
+++ js/user/nguoidung.js
@@ -19,90 +19,106 @@ var member = JSON.parse(localStorage.getItem("member"));
 
     Object.keys(data).forEach((key) => {
       const row = data[key];
-      
+      console.log(member != null);
       if(row && row.email == member){
-      table.innerHTML += `
-        <div class="row">
-        <div class="col-lg-4">
-            <div class="card mb-4">
-            <div class="card-body text-center">
-                <img src="${row.hinhAnh}" alt="avatar"
-                class="rounded-circle img-fluid" style="width: 150px;">
-                <h5 class="my-3">${row.tenKH}</h5>
-                <div id="admin">
-                    ${row.vaiTro == 1?`<p class="text-muted mb-0 text-align-center btn btn-primary " ><a href="admin/index.html" style="color:white;">Admin</a></p>`:""}
-                    
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-8">
-            <div class="card mb-4">
-            <div class="card-body">
+            if(member != null){
+                table.innerHTML += `
                 <div class="row">
-                <div class="col-sm-3">
-                    <p class="mb-0">Họ và Tên</p>
+                <div class="col-lg-4">
+                    <div class="card mb-4">
+                    <div class="card-body text-center">
+                        <img src="${row.hinhAnh}" alt="avatar"
+                        class="rounded-circle img-fluid" style="width: 150px;">
+                        <h5 class="my-3">${row.tenKH}</h5>
+                        <div id="admin">
+                            ${row.vaiTro == 1?`<p class="text-muted mb-0 text-align-center btn btn-primary " ><a href="admin/index.html" style="color:white;">Admin</a></p>`:""}
+                            
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                <div class="col-sm-9">
-                    <p class="text-muted mb-0">${row.tenKH}</p>
+                <div class="col-lg-8">
+                    <div class="card mb-4">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Họ và Tên</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">${row.tenKH}</p>
+                        </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Email</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">${row.email}</p>
+                        </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Số điện thoại</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">${row.sdt}</p>
+                        </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Địa chỉ</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">${row.diaChi}</p>
+                        </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Họ và Tên</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0"><a class="btn btn-primary btn-sm" href="./userchange.html">Chỉnh sửa thông tin</a><a class="btn btn-secondary btn-sm" href="./passchange.html">Đổi mật khẩu</a></p>
+                        </div>
+                        </div>
+                        <hr>
+                    </div>
+                    </div>
+                    <div class="row order_detail">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ID đơn hàng</th>
+                            <th>Ghi chú</th>
+                            <th>Ngày đặt hàng</th>
+                            <th>Trạng thái</th>
+                            <th>Chi tiết</th>
+                            <th>Hành động</th>
+                        </tr>
+                        </thead>
+                        <tbody id="dh">
+                        
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
-                
                 </div>
-                <hr>
-                
-                
-                <div class="row">
-                <div class="col-sm-3">
-                    <p class="mb-0">Email</p>
-                </div>
-                <div class="col-sm-9">
-                    <p class="text-muted mb-0">${row.email}</p>
-                </div>
-                </div>
-                <hr>
-                <div class="row">
-                <div class="col-sm-3">
-                    <p class="mb-0">Số điện thoại</p>
-                </div>
-                <div class="col-sm-9">
-                    <p class="text-muted mb-0">${row.sdt}</p>
-                </div>
-                </div>
-                <hr>
-                <div class="row">
-                <div class="col-sm-3">
-                    <p class="mb-0">Địa chỉ</p>
-                </div>
-                <div class="col-sm-9">
-                    <p class="text-muted mb-0">${row.diaChi}</p>
-                </div>
-                
-                </div>
-            </div>
-            </div>
-            <div class="row order_detail">
-            <table class="table table-bordered">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>ID đơn hàng</th>
-                    <th>Ghi chú</th>
-                    <th>Ngày đặt hàng</th>
-                    <th>Trạng thái</th>
-                    <th>Chi tiết</th>
-                    <th>Hành động</th>
-                </tr>
-                </thead>
-                <tbody id="dh">
-                
-                    
-                
-                </tbody>
-            </table>
-            </div>
-        </div>
-        </div>
-        `;}
+                `;
+            }else{
+                table.innerHTML = `
+                    <div style="text-align: center;">
+        
+                        <h3>Bạn cần phải đăng nhập!</h3>
+                        <a class="btn btn-primary" href="index.html">Về trang chủ</a>
+                    </div>    
+                `
+            }
+        }
+      
         
     });
     Object.keys(data).forEach((key) =>{
--- js/user/userdetail.js
+++ js/user/userdetail.js
@@ -1,15 +1,11 @@
 let member = document.querySelector("#khachHang");
 
 let render = `
-        <div style="text-align: center;">
-
-            <h3>Bạn cần phải đăng nhập!</h3>
-            <a class="btn btn-primary" href="index.html">Về trang chủ</a>
-        </div>    
+        
 `
 var checkMember = JSON.parse(localStorage.getItem("member"));
 if(checkMember == null){
-    member.innerHTML += render;
+    // member.innerHTML += render;
 }else{
-    member.innerHTML =``;
+    // member.innerHTML =``;
 }
